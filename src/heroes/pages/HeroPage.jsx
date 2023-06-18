e189ebbfbcf0f10b2aa210edd25ed62e2ab3cce1
Facundo Vandecaveye <facundovs84@hotmail.com.ar>
2023-06-17 22:04:14 -0300

    Responsive en vista de hero

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -23,20 +23,31 @@ export const HeroPage = () => {
     
     return (
         <div className="row mt-5">
-            <div className="col-4">
-                <img src={ `/heroes/${heroId}.jpg`} alt={hero.superhero} className="img-thumbnail animate__animated animate__fadeInLeft"/>
+            <div className="col-12 col-md-4 mb-3">
+                <img src={`/heroes/${heroId}.jpg`} alt={hero.superhero} className="img-thumbnail animate__animated animate__fadeInLeft" />
             </div>
-            <div className="col-8">
-                <h3>{ hero.superhero }</h3>
+            <div className="col-12 col-md-8">
+                <h3>{hero.superhero}</h3>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item"><b>Alter ego: </b>{hero.alter_ego}</li>
-                    <li className="list-group-item"><b>Publisher: </b>{hero.publisher}</li>
-                    <li className="list-group-item"><b>First appearance: </b>{hero.first_appearance}</li>
+                    <li className="list-group-item">
+                        <b>Alter ego: </b>
+                        {hero.alter_ego}
+                    </li>
+                    <li className="list-group-item">
+                        <b>Publisher: </b>
+                        {hero.publisher}
+                    </li>
+                    <li className="list-group-item">
+                        <b>First appearance: </b>
+                        {hero.first_appearance}
+                    </li>
                 </ul>
                 <h5 className="mt-3">Characters</h5>
                 <p>{hero.characters}</p>
 
-                <button className="btn btn-outline-primary" onClick={onNavigateBack}>Regresar</button>
+                <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+                    Regresar
+                </button>
             </div>
         </div>
     )
